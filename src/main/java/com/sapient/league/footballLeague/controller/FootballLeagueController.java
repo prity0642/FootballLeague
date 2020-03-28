c0f704cdbdd89048c08f15cf686b9ed12b36efe4
prity0642 <41786806+prity0642@users.noreply.github.com>
2020-03-28 13:41:17 +0530

    updated files

--- src/main/java/com/sapient/league/footballLeague/controller/FootballLeagueController.java
+++ src/main/java/com/sapient/league/footballLeague/controller/FootballLeagueController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.sapient.league.footballLeague.entity.FootballLeague;
 import com.sapient.league.footballLeague.service.FootballLeagueService;
@@ -27,9 +28,10 @@ public class FootballLeagueController {
 	FootballLeagueService footballLeagueService;
 	
 	@GetMapping(value="/summary")
-	public ResponseEntity<List<FootballLeague>> leagueSummary(){
+	public ResponseEntity<List<FootballLeague>> leagueSummary(@RequestParam(required = true)  String id){
 		
-		List<FootballLeague> footballLeagues = footballLeagueService.getLeagueData(url+apiKey);
+		
+		List<FootballLeague> footballLeagues = footballLeagueService.getLeagueData(url+"&league_id="+id+"&APIkey="+apiKey);
 		
 		return new ResponseEntity<List<FootballLeague>>(footballLeagues,HttpStatus.OK);
 	}
--- src/test/java/com/sapient/league/footballLeague/FootballLeagueApplicationTests.java
+++ src/test/java/com/sapient/league/footballLeague/FootballLeagueApplicationTests.java
@@ -1,13 +1,68 @@
 package com.sapient.league.footballLeague;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.assertSame;
 
-@SpringBootTest
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestContext;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.client.RestTemplate;
+
+import com.sapient.league.footballLeague.controller.FootballLeagueController;
+import com.sapient.league.footballLeague.entity.FootballLeague;
+import com.sapient.league.footballLeague.service.FootballLeagueService;
+
+@RunWith(MockitoJUnitRunner.class)
+@ContextConfiguration(classes = {TestContext.class})
+@WebAppConfiguration
 class FootballLeagueApplicationTests {
+	
+	@InjectMocks
+	FootballLeagueController footballController=new FootballLeagueController();
+	
+	@Mock
+	FootballLeagueService footballLeagueService =new FootballLeagueService();
 
-	@Test
-	void contextLoads() {
+	@Mock
+	RestTemplate restTemplate = new RestTemplate();
+	
+
+	
+	//@Test
+	void testLeagueSummary() {
+		FootballLeague footballLeague = new FootballLeague();
+		footballLeague.setCountry_name("England");
+		footballLeague.setLeague_id("148");
+		footballLeague.setLeague_name("");
+		footballLeague.setOverall_league_position("1");
+		footballLeague.setTeam_id("123");
+		footballLeague.setTeam_name("test");
+		
+		FootballLeague[] fbl = {footballLeague};
+		
+		List<FootballLeague> footballLeagues = new ArrayList<>();
+		
+		footballLeagues.add(footballLeague);
+		
+	   String url ="https://apiv2.apifootball.com/?action=get_standings&league_id=148&APIkey=9bb66184e0c8145384fd2cc0f7b914ada57b4e8fd2e4d6d586adcc27c257a978";
+		
+ 		Mockito.when(footballLeagueService.getLeagueData(url)).thenReturn(footballLeagues);	
+ 		Mockito.when(restTemplate.getForObject(url,FootballLeague[].class)).thenReturn(fbl);
+		
+		ResponseEntity<List<FootballLeague>> result =footballController.leagueSummary("148");
+		
+		
+		
+		assertSame(result.getStatusCode(), HttpStatus.OK);
 	}
 
 }
